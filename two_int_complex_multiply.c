9175eb4ade090fd437da718861439f9373b16d40
Xingguang Wei <jxweixingguang@163.com>
2016-03-21 22:07:17 +0800

    finish the two int complex multiply function

--- two_int_complex_multiply.c
+++ two_int_complex_multiply.c
@@ -1,5 +1,10 @@
+/*
+* compile instruction: gcc two_int_complex_multiply.c -o two_int_complex_multiply -msse4.1
+*/
 #include <stdio.h>
+#include <stdlib.h>
 #include "emmintrin.h"
+#include "tmmintrin.h"
 #include <stdint.h>
 
 int16_t conjugate[8] __attribute__((aligned(16))) = {1,-1,1,-1,1,-1,1,-1};
@@ -39,7 +44,52 @@ void two_int_complex_multiply(int16_t *inputa, int16_t *inputb, int length, int1
 }
 int main(int argc, char *argv[])
 {
+	int shift = 9;
+	int data_length = 16;
+	int *inputa = malloc( data_length * sizeof(int));
+	int *inputb = malloc( data_length * sizeof(int));
+	int *output = malloc( data_length * sizeof(int));
+
+	int index;
+	for(index = 0; index < data_length; index++)
+	{
+		((int16_t *)&inputa[index])[0] = rand() % 512;
+		((int16_t *)&inputa[index])[1] = rand() % 512;
+		((int16_t *)&inputb[index])[0] = rand() % 512;
+		((int16_t *)&inputb[index])[1] = rand() % 512;
+	}
+
+	puts("Input data A:");
+	for(index = 0; index < data_length; index++)
+	{
+		if(index % 10 == 9)
+			puts("");
+
+		printf("(%d %d) ",((int16_t *)&inputa[index])[0],((int16_t *)&inputa[index])[1]);
+	}
+	puts("\n");
 
 
+	puts("Input data B:");
+	for(index = 0; index < data_length; index++)
+	{
+		if(index % 10 == 9)
+			puts("");
+
+		printf("(%d %d) ",((int16_t *)&inputb[index])[0],((int16_t *)&inputb[index])[1]);
+	}
+	puts("\n");
+
+	two_int_complex_multiply((int16_t *)inputa, (int16_t *)inputb, data_length, (int16_t *)output, shift);
+
+	puts("output data:");
+	for(index = 0; index < data_length; index++)
+	{
+		if(index % 10 == 9)
+			puts("");
+
+		printf("(%d %d) ",((int16_t *)&output[index])[0],((int16_t *)&output[index])[1]);
+	}
+	puts("");
 	return 0;
 }
